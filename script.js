6212f5e1a0f7f9b5bd4c72cb425e154b514f5970
ECyborg <lelynho73@gmail.com>
2024-07-10 11:53:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var divDaImg = document.querySelector('.div-da-img');
+
+    setTimeout(function() {
+        divDaImg.style.opacity = '2';
+        document.body.style.overflow = 'auto'; 
+    }, 200);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var imagemp = document.querySelector('#imagemp');
+
+    setTimeout(function() {
+        imagemp.style.opacity = '2';
+        document.body.style.overflow = 'auto'; 
+    }, 200);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var caixadoinput = document.querySelector('.caixa-do-input');
+
+    setTimeout(function() {
+        caixadoinput.style.opacity = '2';
+        document.body.style.overflow = 'auto'; 
+    }, 200);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var container = document.querySelector('.container');
+
+    setTimeout(function() {
+        container.style.opacity = '2';
+        document.body.style.overflow = 'auto'; 
+    }, 200);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var imagecontainerimg3 = document.querySelector('#image-container-img3');
+
+    setTimeout(function() {
+        imagecontainerimg3.style.opacity = '2';
+        document.body.style.overflow = 'auto'; 
+    }, 200);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var message2 = document.querySelector('.message2');
+
+    setTimeout(function() {
+        message2.style.opacity = '2';
+        document.body.style.overflow = 'auto'; 
+    }, 200);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var ultimadiv = document.querySelector('.ultima-div');
+
+    setTimeout(function() {
+        ultimadiv.style.opacity = '2';
+        document.body.style.overflow = 'auto'; 
+    }, 200);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var footer = document.querySelector('footer');
+
+    setTimeout(function() {
+        footer.style.opacity = '2';
+        document.body.style.overflow = 'auto'; 
+    }, 200);
+});
